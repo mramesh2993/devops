14572781d1af8c6e29f5a73aedb6d88b35556d09
rameshmari <51149364+mramesh2993@users.noreply.github.com>
2019-11-07 15:39:25 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,9 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("This will be printed- Ramesh");
+
+        System.out.println("Goodbye, World!");
+
     }
+
 }
